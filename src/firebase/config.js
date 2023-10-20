adc870b4bd051a0c5f8b34c0941cd77fed74a6d8
paulinacartagena1234 <paulinacartagena1234@gmail.com>
2023-10-19 20:20:29 -0500

    firebase

---
+++ src/firebase/config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAYYD3sDttSf3Gm9ad7Er1-N6IILI5bxUw",
+    authDomain: "dbpaginaappgym.firebaseapp.com",
+    databaseURL: "https://dbpaginaappgym-default-rtdb.firebaseio.com",
+    projectId: "dbpaginaappgym",
+    storageBucket: "dbpaginaappgym.appspot.com",
+    messagingSenderId: "274680114539",
+    appId: "1:274680114539:web:b19a4b7feb698c10a8302e"
+};
+
+export default firebaseConfig;
