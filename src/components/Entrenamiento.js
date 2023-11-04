650a71d432e962ac29db8213bd9bc2ed22c21282
AndrelGanShiTV <andresfelipegiraldoandrade@gmail.com>
2023-11-04 18:49:38 -0500

    aaaa

--- src/components/Entrenamiento.js
+++ src/components/Entrenamiento.js
@@ -1,14 +1,15 @@
-import React from 'react'
+import React, {useState, useEffect, useContext} from 'react'
+import { FirebaseContext } from '../firebase';
 
-function Entrenamiento() {
+function Entrenamiento({}) {
+    const {firebase} = useContext(FirebaseContext);
     return (
         <div class=''>
             <h1 className='text-gray-600 '>Entrenamiento</h1>
-
+            
             
         </div>
     )
 }
 
-export default Entrenamiento
-
+export default Entrenamiento;
--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,13 +1,21 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from "react";
+import { Link } from "react-router-dom";
+import VerEntrenamiento from "../ui/VerEntrenamiento";
 
 function Menu() {
-    return (
-        <>
-            <h1 className=''>Bienvenido a SUPER BODY</h1>
-            <h2><Link to="/Register">Registro</Link></h2>
-        </>
-    )
+  return (
+    <>
+      <h1 className="text-sky-600 m-10 font-light text-3xl">
+        Bienvenido a SUPER BODY
+      </h1>
+      <Link to="/Register">
+        <h1 className="ml-3 bg-blue-800 hover:bg-blue-400, inline-block mb-5 p-2 text-white uppercase font-bold">
+          Registro
+        </h1>
+      </Link>
+      <VerEntrenamiento />
+    </>
+  );
 }
 
-export default Menu
+export default Menu;
